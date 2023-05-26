87ee5b1c56164b9901d7a03f84ce24a8a452b9e0
Knyaz007 <113051589+Knyaz007@users.noreply.github.com>
2023-05-26 19:33:10 +0300

    26ю05_1932

--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@ namespace Kursovaay.Models
 {
     public class User
     {
+
         public int UserId { get; set; } // Идентификатор пользователя
         public string Username { get; set; } // Имя пользователя
         public string Password { get; set; } // Пароль пользователя
