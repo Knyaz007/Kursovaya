20d4a984707363cf65dd473f9dca1dad56df124d
Knyaz007 <113051589+Knyaz007@users.noreply.github.com>
2023-06-05 11:04:46 +0300

    Добавил отчет к отелям

--- Controllers/HotelsController.cs
+++ Controllers/HotelsController.cs
@@ -6,16 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Kursovaya.Models;
+using Microsoft.CodeAnalysis;
+using System.ComponentModel;
+using System.Numerics;
+using OfficeOpenXml;
 
 namespace Kursovaya.Controllers
 {
     public class HotelsController : Controller
     {
         private readonly TravAgenDBContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-        public HotelsController(TravAgenDBContext context)
+        public HotelsController(TravAgenDBContext context, IWebHostEnvironment appEnvironment)
         {
             _context = context;
+            _appEnvironment = appEnvironment;
         }
 
         // GET: Hotels
@@ -157,6 +163,62 @@ namespace Kursovaya.Controllers
         private bool HotelExists(int id)
         {
           return (_context.Hotels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+        public FileResult GetReport()
+        {
+            // Путь к файлу с шаблоном
+            string path = "/Reports/report_template.xlsx";
+            //Путь к файлу с результатом
+            string result = "/Reports/Holetels.xlsx";
+            FileInfo fi = new FileInfo(_appEnvironment.WebRootPath + path);
+            FileInfo fr = new FileInfo(_appEnvironment.WebRootPath + result);
+            //будем использовть библитотеку не для коммерческого использования
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            //открываем файл с шаблоном
+            using (ExcelPackage excelPackage = new ExcelPackage(fi))
+            {
+                //устанавливаем поля документа
+                excelPackage.Workbook.Properties.Author = "Хпбалов В.А.";
+                excelPackage.Workbook.Properties.Title = "Список Отелей";
+                excelPackage.Workbook.Properties.Subject = "Отели";
+                excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                //плучаем лист по имени.
+                ExcelWorksheet worksheet =
+                    excelPackage.Workbook.Worksheets["Hotels"];
+                //получаем списко пользователей и в цикле заполняем лист данными
+                int startLine = 3;
+                List<Hotel> hotels = _context.Hotels.ToList();
+                foreach (Hotel hotel in hotels)
+                {
+                    //List<Specialty> specialties = doctor.Specialties.ToList();
+                    worksheet.Cells[startLine, 1].Value = startLine - 2;
+                    worksheet.Cells[startLine, 2].Value = hotel.Name;
+                    worksheet.Cells[startLine, 3].Value = hotel.Address;
+                    worksheet.Cells[startLine, 4].Value = hotel.AvailableRooms;
+                    //worksheet.Cells[startLine, 5].Value = hotel.WorkExperience;
+                    //if (specialties != null)
+                    //{
+                    //    string[] strSp = new string[specialties.Count];
+                    //    int i = 0;
+                    //    foreach (Specialty specialty in specialties)
+                    //    {
+                    //        strSp[i] = specialty.NameSpecialty;
+                    //        i++;
+                    //    }
+                    //    worksheet.Cells[startLine, 6].Value = String.Join(", ", strSp);
+                    //}
+                    startLine++;
+                }
+                //созраняем в новое место
+                excelPackage.SaveAs(fr);
+                // Тип файла - content-type
+                string file_type =
+                    "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet";
+                // Имя файла - необязательно
+                string file_name = "reportHotels.xlsx";
+                return File(result, file_type, file_name);
+            }
         }
     }
 }
